232bafb56444c66ce73818630d193fdde74f34ac
Lacuran <kornel.wrzesien@gmail.com>
2025-06-24 11:26:53 +0200

    Added db connection

--- src/main/java/bank/DataSource.java
+++ src/main/java/bank/DataSource.java
@@ -6,10 +6,8 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 
-
+@NoArgsConstructor
 public class DataSource {
-
-    DataSource(){}
 
     public static Connection connect() {
         String db_file = "jdbc:sqlite:resources/bank.db";
