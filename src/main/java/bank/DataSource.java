180b6fb4ae1d3e4341eae5efabc041864e4dd8f7
Lacuran <kornel.wrzesien@gmail.com>
2025-06-24 11:25:32 +0200

    Added db connection

--- src/main/java/bank/DataSource.java
+++ src/main/java/bank/DataSource.java
@@ -1,10 +1,27 @@
 package bank;
 
+import lombok.NoArgsConstructor;
+
 import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 
 public class DataSource {
 
-    public static Connection connect(){
+    DataSource(){}
 
+    public static Connection connect() {
+        String db_file = "jdbc:sqlite:resources/bank.db";
+        Connection connection = null;
+
+        try {
+            connection = DriverManager.getConnection(db_file);
+            System.out.println("Connected");
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+        return connection;
     }
 }
